2084ffe64fc76e2cc09e2695d81f2ef284dbfad9
abadawi2 <abadawi@terpmail.umd.edu>
2018-07-26 19:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function happyHolidays() {
-  return "Happy holidays!"
+function happyHolidays (){
+  return "Happy holdays!"
 }
 
-function happyHolidaysTo(name){
-return `Happy holidays, ${name}!`
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo (holiday,name){
+function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (holiday, days){
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${days} until ${holiday}!`
 }
